caab6a3570085ffef4d79080a56599d98286e154
Regina Caelia <regina.caelia18@gmail.com>
2026-03-25 22:51:50 +0700

    hapus folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { ReactNode } from "react";
-import "./globals.css";
 import { ToastContainer } from "react-toastify";
 export const metadata: Metadata = {
   title: "Auth System",
